01ddc655bf72402ee7bd40d4aec24ed84657394a
Ryan Stong <rvanstong@gmail.com>
2016-10-28 15:27:30 -0700

    implements regex instead of split method for marklar

--- exercise.rb
+++ exercise.rb
@@ -1,11 +1,15 @@
 class Exercise
-
   # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
+    str.gsub!(/[A-Za-z]+{5,}/, "marklar")
+    str.gsub!(/[A-Z][a-z]+{5,}/, "Marklar")
+    str
   end
+
+
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
   # the "nth" term in the sequence
@@ -14,5 +18,6 @@ class Exercise
   def self.even_fibonacci(nth)
     # TODO: Implement this method
   end
+
 
 end
